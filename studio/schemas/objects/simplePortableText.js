da6819813db0c3b12b2a6185eb0e679a11a2c88c
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-10-29 08:53:11 -0700

    add link to schema

--- studio/schemas/objects/simplePortableText.js
+++ studio/schemas/objects/simplePortableText.js
@@ -32,7 +32,28 @@ export default {
           { title: 'Code', value: 'code' },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
-        annotations: [],
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'External link',
+            fields: [
+              {
+                name: 'href',
+                type: 'url',
+                title: 'URL',
+                validation: Rule =>
+                  Rule.uri({ scheme: ['http', 'https', 'mailto', 'tel'] }),
+              },
+              {
+                title: 'Open in new tab',
+                name: 'blank',
+                description: 'Read https://css-tricks.com/use-target_blank/',
+                type: 'boolean',
+              },
+            ],
+          },
+        ],
       },
     },
   ],
